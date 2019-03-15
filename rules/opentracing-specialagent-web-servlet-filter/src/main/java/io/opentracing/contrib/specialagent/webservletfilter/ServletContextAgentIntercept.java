9fe0a080b9d517f3feb687d67c1e7e0ad3094cd8
iBranch211 <leo.esaki211@gmail.com>
2019-03-15 14:57:25 +0100

    Conform webservletfilter

--- rules/opentracing-specialagent-web-servlet-filter/src/main/java/io/opentracing/contrib/specialagent/webservletfilter/ServletContextAgentIntercept.java
+++ rules/opentracing-specialagent-web-servlet-filter/src/main/java/io/opentracing/contrib/specialagent/webservletfilter/ServletContextAgentIntercept.java
@@ -17,16 +17,23 @@ package io.opentracing.contrib.specialagent.webservletfilter;
 
 import java.util.EnumSet;
 
-import org.apache.catalina.core.ApplicationContext;
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
 
 import io.opentracing.contrib.web.servlet.filter.TracingFilter;
 import io.opentracing.util.GlobalTracer;
 
-public class TomcatAgentIntercept {
+public class ServletContextAgentIntercept {
+  private static final String[] patterns = {"/*"};
+
   public static void exit(final Object thiz) {
-    final ApplicationContext context = (ApplicationContext)thiz;
+    if (!(thiz instanceof ServletContext))
+      return;
+
     final TracingFilter filter = new TracingFilter(GlobalTracer.get());
-    final String[] patterns = {"/*"};
-    context.addFilter("tracingFilter", filter).addMappingForUrlPatterns(EnumSet.allOf(javax.servlet.DispatcherType.class), true, patterns);
+    final FilterRegistration.Dynamic registration = ((ServletContext)thiz).addFilter("tracingFilter", filter);
+    if (registration != null)
+      registration.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, patterns);
   }
 }
