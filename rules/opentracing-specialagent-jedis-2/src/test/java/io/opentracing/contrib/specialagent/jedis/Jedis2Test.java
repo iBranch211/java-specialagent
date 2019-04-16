e4ada3a8aac7134665bbf179b3feef87528c62cd
iBranch211 <leo.esaki211@gmail.com>
2019-04-16 22:32:02 +0800

    Add Spring RabbitMQ rule

--- rules/opentracing-specialagent-jedis-2/src/test/java/io/opentracing/contrib/specialagent/jedis/Jedis2Test.java
+++ rules/opentracing-specialagent-jedis-2/src/test/java/io/opentracing/contrib/specialagent/jedis/Jedis2Test.java
@@ -12,23 +12,22 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package io.opentracing.contrib.specialagent.jedis;
 
-import static org.junit.Assert.*;
-
-import java.io.IOException;
-import java.util.List;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 import io.opentracing.contrib.specialagent.AgentRunner;
 import io.opentracing.mock.MockSpan;
 import io.opentracing.mock.MockTracer;
 import io.opentracing.tag.Tags;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Pipeline;
 import redis.embedded.RedisServer;
@@ -39,43 +38,43 @@ public class Jedis2Test {
   private Jedis jedis;
 
   @Before
-  public void before(final MockTracer tracer) throws IOException {
+  public void before(final MockTracer tracer) {
     tracer.reset();
 
-    redisServer = new RedisServer();
+    redisServer = RedisServer.builder().setting("bind 127.0.0.1").build();
     redisServer.start();
     jedis = new Jedis();
   }
 
   @After
   public void after() {
-    if (redisServer != null)
+    if (redisServer != null) {
       redisServer.stop();
-
-    if (jedis != null)
+    }
+    if (jedis != null) {
       jedis.close();
+    }
   }
 
   @Test
-  public void test(final MockTracer tracer) {
+  public void test(MockTracer tracer) {
     assertEquals("OK", jedis.set("key", "value"));
     assertEquals("value", jedis.get("key"));
 
-    final List<MockSpan> spans = tracer.finishedSpans();
+    List<MockSpan> spans = tracer.finishedSpans();
     assertEquals(2, spans.size());
     checkSpans(spans);
   }
 
   @Test
-  public void withError(final MockTracer tracer) {
+  public void withError(MockTracer tracer) {
     try {
       jedis.eval("bla-bla-bla");
       fail();
-    }
-    catch (final Exception ignore) {
+    } catch (Exception ignore) {
     }
 
-    final List<MockSpan> spans = tracer.finishedSpans();
+    List<MockSpan> spans = tracer.finishedSpans();
     assertEquals(1, spans.size());
     assertEquals(Boolean.TRUE, spans.get(0).tags().get(Tags.ERROR.getKey()));
     assertFalse(spans.get(0).logEntries().isEmpty());
@@ -83,19 +82,19 @@ public class Jedis2Test {
   }
 
   @Test
-  public void pipeline(final MockTracer tracer) {
-    final Pipeline pipeline = jedis.pipelined();
-    for (int i = 0; i < 5; ++i)
-      pipeline.set("key-" + i, "value-" + i);
-
-    pipeline.sync();
-    final List<MockSpan> spans = tracer.finishedSpans();
+  public void pipeline(MockTracer tracer) {
+    Pipeline p = jedis.pipelined();
+    for (int i = 0; i < 5; i++) {
+      p.set("key-" + i, "value-" + i);
+    }
+    p.sync();
+    List<MockSpan> spans = tracer.finishedSpans();
     assertEquals(5, spans.size());
     checkSpans(spans);
   }
 
-  private static void checkSpans(final List<MockSpan> spans) {
-    for (final MockSpan span : spans) {
+  private void checkSpans(List<MockSpan> spans) {
+    for (MockSpan span : spans) {
       assertEquals("java-redis", span.tags().get(Tags.COMPONENT.getKey()));
       assertEquals("redis", span.tags().get(Tags.DB_TYPE.getKey()));
       assertEquals(Tags.SPAN_KIND_CLIENT, span.tags().get(Tags.SPAN_KIND.getKey()));
