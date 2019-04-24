f6aa62b3fdb33a875c3e0aa6296077f3d0e6d3bf
iBranch211 <leo.esaki211@gmail.com>
2019-04-24 22:17:58 +0800

    upgrade LS tracer to 0.16.0 version which is based on OT API 0.32.0

--- rules/opentracing-specialagent-okhttp/src/main/java/io/opentracing/contrib/specialagent/okhttp/OkHttpAgentIntercept.java
+++ rules/opentracing-specialagent-okhttp/src/main/java/io/opentracing/contrib/specialagent/okhttp/OkHttpAgentIntercept.java
@@ -15,28 +15,17 @@
 
 package io.opentracing.contrib.specialagent.okhttp;
 
-import java.util.ArrayList;
 import java.util.Collections;
 
 import io.opentracing.contrib.okhttp3.OkHttpClientSpanDecorator;
 import io.opentracing.contrib.okhttp3.TracingInterceptor;
 import io.opentracing.util.GlobalTracer;
-import java.util.List;
-import okhttp3.Interceptor;
 
 public class OkHttpAgentIntercept {
-
-  @SuppressWarnings("unchecked")
-  public static Object exit(final Object returned) {
-    List<Interceptor> interceptors = (List<Interceptor>) returned;
-    for (Interceptor interceptor : interceptors) {
-      if(interceptor instanceof TracingInterceptor) {
-        return returned;
-      }
-    }
-    List<Interceptor> newInterceptors = new ArrayList<>(interceptors);
+  public static void enter(final Object thiz) {
+    final okhttp3.OkHttpClient.Builder builder = (okhttp3.OkHttpClient.Builder)thiz;
     final TracingInterceptor interceptor = new TracingInterceptor(GlobalTracer.get(), Collections.singletonList(OkHttpClientSpanDecorator.STANDARD_TAGS));
-    newInterceptors.add(0, interceptor);
-    return Collections.unmodifiableList(newInterceptors);
+    builder.addInterceptor(interceptor);
+    builder.addNetworkInterceptor(interceptor);
   }
 }
