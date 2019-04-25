68b3dd1a0d1ee4216daaca4d44f88c293619ed5d
iBranch211 <leo.esaki211@gmail.com>
2019-04-25 11:43:19 +0200

    Conform Apache HttpClient Plugin

---
+++ rules/opentracing-specialagent-apache-httpclient/src/main/java/io/opentracing/contrib/specialagent/httpclient/HttpClientAgentIntercept.java
@@ -0,0 +1,114 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.httpclient;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpUriRequest;
+
+import io.opentracing.Span;
+import io.opentracing.propagation.Format.Builtin;
+import io.opentracing.tag.Tags;
+import io.opentracing.util.GlobalTracer;
+
+public class HttpClientAgentIntercept {
+  private static class Context {
+    private int counter = 1;
+    private Span span;
+  }
+
+  static final String COMPONENT_NAME = "java-httpclient";
+  private static final ThreadLocal<Context> contextHolder = new ThreadLocal<>();
+
+  public static Object[] enter(final Object arg0, final Object arg1, final Object arg2) {
+    final HttpRequest request = arg0 instanceof HttpRequest ? (HttpRequest)arg0 : arg1 instanceof HttpRequest ? (HttpRequest)arg1 : null;
+    if (request == null)
+      return null;
+
+    Context context = contextHolder.get();
+    if (context != null) {
+      ++context.counter;
+      return null;
+    }
+
+    context = new Context();
+    contextHolder.set(context);
+
+    final Span span = createSpan(request);
+    if (request instanceof HttpUriRequest) {
+      final HttpUriRequest uriRequest = (HttpUriRequest)request;
+      Tags.PEER_HOSTNAME.set(span, uriRequest.getURI().getHost());
+      Tags.PEER_PORT.set(span, uriRequest.getURI().getPort() == -1 ? 80 : uriRequest.getURI().getPort());
+    }
+
+    GlobalTracer.get().inject(span.context(), Builtin.HTTP_HEADERS, new HttpHeadersInjectAdapter(request));
+    context.span = span;
+    if (arg1 instanceof ResponseHandler)
+      return new Object[] {new TracingResponseHandler<>((ResponseHandler<?>)arg1, span)};
+
+    if (arg2 instanceof ResponseHandler)
+      return new Object[] {null, new TracingResponseHandler<>((ResponseHandler<?>)arg2, span)};
+
+    return null;
+  }
+
+  private static Span createSpan(final HttpRequest request) {
+    return GlobalTracer.get()
+      .buildSpan(request.getRequestLine().getMethod())
+      .withTag(Tags.COMPONENT, COMPONENT_NAME)
+      .withTag(Tags.HTTP_METHOD, request.getRequestLine().getMethod())
+      .withTag(Tags.HTTP_URL, request.getRequestLine().getUri()).start();
+  }
+
+  public static void exit(final Object returned) {
+    final Context context = contextHolder.get();
+    if (context == null)
+      return;
+
+    --context.counter;
+    if (context.counter == 0) {
+      if (returned instanceof HttpResponse) {
+        final HttpResponse response = (HttpResponse)returned;
+        Tags.HTTP_STATUS.set(context.span, response.getStatusLine().getStatusCode());
+      }
+
+      context.span.finish();
+      contextHolder.remove();
+    }
+  }
+
+  public static void onError(Throwable thrown) {
+    final Context context = contextHolder.get();
+    if (context == null)
+      return;
+
+    --context.counter;
+    if (context.counter == 0) {
+      Tags.ERROR.set(context.span, Boolean.TRUE);
+
+      final Map<String,Object> errorLogs = new HashMap<>(2);
+      errorLogs.put("event", Tags.ERROR.getKey());
+      errorLogs.put("error.object", thrown);
+      context.span.log(errorLogs);
+      context.span.finish();
+      contextHolder.remove();
+    }
+  }
+}
---
+++ rules/opentracing-specialagent-apache-httpclient/src/main/java/io/opentracing/contrib/specialagent/httpclient/HttpClientAgentRule.java
@@ -0,0 +1,77 @@
+/* Copyright 2019 The OpenTracing Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.opentracing.contrib.specialagent.httpclient;
+
+import static net.bytebuddy.matcher.ElementMatchers.*;
+
+import java.util.Arrays;
+
+import io.opentracing.contrib.specialagent.AgentRule;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.agent.builder.AgentBuilder.Identified.Narrowable;
+import net.bytebuddy.agent.builder.AgentBuilder.Transformer;
+import net.bytebuddy.asm.Advice;
+import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.dynamic.DynamicType.Builder;
+import net.bytebuddy.implementation.bytecode.assign.Assigner.Typing;
+import net.bytebuddy.utility.JavaModule;
+
+public class HttpClientAgentRule extends AgentRule {
+  @Override
+  public Iterable<? extends AgentBuilder> buildAgent(final String agentArgs, final AgentBuilder builder) {
+    final Narrowable narrowable = builder.type(hasSuperType(named("org.apache.http.client.HttpClient")));
+    return Arrays.asList(narrowable.transform(new Transformer() {
+      @Override
+      public Builder<?> transform(final Builder<?> builder, final TypeDescription typeDescription, final ClassLoader classLoader, final JavaModule module) {
+        return builder.visit(Advice.to(HttpClientAgentRule.class).on(named("execute")));
+      }
+    }), narrowable.transform(new Transformer() {
+      @Override
+      public Builder<?> transform(Builder<?> builder, TypeDescription typeDescription, ClassLoader classLoader, JavaModule module) {
+        return builder.visit(Advice.to(OnException.class).on(named("execute")));
+      }
+    }));
+  }
+
+  @Advice.OnMethodEnter
+  public static void enter(final @Advice.Origin String origin, final @Advice.Argument(value = 0, typing = Typing.DYNAMIC) Object arg0, @Advice.Argument(value = 1, optional = true, readOnly = false, typing = Typing.DYNAMIC) Object arg1, @Advice.Argument(value = 2, optional = true, readOnly = false, typing = Typing.DYNAMIC) Object arg2) {
+    if (!isEnabled(origin))
+      return;
+
+    final Object[] objects = HttpClientAgentIntercept.enter(arg0, arg1, arg2);
+    if (objects == null)
+      return;
+
+    if (objects.length == 1)
+      arg1 = objects[0];
+    else if (objects.length == 2)
+      arg2 = objects[1];
+  }
+
+  @Advice.OnMethodExit
+  public static void exit(final @Advice.Origin String origin, final @Advice.Return(typing = Typing.DYNAMIC) Object returned) {
+    if (isEnabled(origin))
+      HttpClientAgentIntercept.exit(returned);
+  }
+
+  public static class OnException {
+    @Advice.OnMethodExit(onThrowable = Throwable.class)
+    public static void exit(final @Advice.Thrown(typing = Typing.DYNAMIC) Throwable thrown) {
+      if (thrown != null)
+        HttpClientAgentIntercept.onError(thrown);
+    }
+  }
+}
