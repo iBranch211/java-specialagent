a92b060e8802bd8763ae94c531e811b5ef7bc170
iBranch211 <leo.esaki211@gmail.com>
2020-02-17 11:11:48 -0800

    Conform patterns, re #419 [skip ci]

--- opentracing-specialagent-util/src/main/java/io/opentracing/contrib/specialagent/tracer/SpanRuleMatcher.java
+++
@@ -1,8 +0,0 @@
-package io.opentracing.contrib.specialagent.tracer;
-
-public interface SpanRuleMatcher {
-  /**
-   * returns {@code null} if not matched
-   */
-  SpanRuleMatch match(Object value);
-}
